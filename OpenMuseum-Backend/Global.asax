ba38d35bfa4034aed19f76f0e02e3d441aca804c
okolobaxa <okolobaxa@gmail.com>
2015-08-23 20:46:58 +0300

    Initial commit

---
+++ OpenMuseum-Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenMuseum_Backend.Global" Language="C#" %>
---
+++ OpenMuseum-Backend/Models/Layer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace OpenMuseum_Backend.Models
+{
+    public class Layer
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("width")]
+        public int Width { get; set; }
+        [JsonProperty("url")]
+        public string TileUrl { get; set; }
+    }
+}
