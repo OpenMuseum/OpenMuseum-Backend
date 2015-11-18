b7101fda93185ce8aa8612f949c5cf920d1b7719
Anton Kheystver <okolobaxa@gmail.com>
2015-11-18 23:01:26 +0300

    add UniqId

--- OpenMuseum.Models/BaseLayer.cs
+++ OpenMuseum.Models/BaseLayer.cs
@@ -10,10 +10,11 @@ namespace OpenMuseum.Backend.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Url { get; set; }
+        public Guid UniqId { get; set; }
         public bool Default { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
+        public string Url { get; set; }
 
         public virtual ICollection<DataLayer> DataLayers { get; set; }
     }
